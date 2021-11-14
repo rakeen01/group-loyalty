9f37f85a2ca8394de1126babb494328ad819287e
Mahathir Hossan Abir Maxim <56098622+mahathir-maxim@users.noreply.github.com>
2021-11-13 23:10:35 -0600

    tabs added

--- group-loyalty-app/src/components/Dashboard.jsx
+++ group-loyalty-app/src/components/Dashboard.jsx
@@ -1,29 +1,31 @@
 import React from "react";
+import Box from '@mui/material/Box';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
 
 function Dashboard() {
     //
     //
-  return (
-    <div className="dashboard">
-      <div class="container">
-        <div class="row align-items-center my-5">
-          <div class="col-lg-7">
-            <img
-              class="img-fluid rounded mb-4 mb-lg-0"
-              src="http://placehold.it/900x400"
-              alt=""
-            />
-          </div>
-          <div class="col-lg-5">
-            <h1 class="font-weight-light">Home</h1>
-            <p>
-              Rakeen has a small dick
-            </p>
-          </div>
+    const [value, setValue] = React.useState(0);
+
+    const handleChange = (event, newValue) => {
+    setValue(newValue);
+    };
+
+    return (
+        <div className="dashboard">
+        <div class="container">
+            <div class="row align-items-center my-5">
+            <Box sx={{ width: '100%', bgcolor: 'background.paper' }}>
+                <Tabs value={value} onChange={handleChange} centered>
+                    <Tab label="Personal" />
+                    <Tab label="Group" />
+                </Tabs>
+            </Box>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+        </div>
+    );
 }
 
 export default Dashboard;
